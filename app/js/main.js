aa677c6312705b07d692a9b0e45241bc6259d622
callmegrisha <callmegrisha@gmail.com>
2020-11-26 00:14:47 +0200

    added slider and tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function() {
 
+    $('.slider__inner').slick({
+        arrows: false,
+        dots: true
+    });
+
+    $('.tab').on('click', function(e) {
+        e.preventDefault();
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+    });
+
 });
