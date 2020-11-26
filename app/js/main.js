1eb0b52e319fe298c0e7cf9c3c425ec61f96c4bf
callmegrisha <callmegrisha@gmail.com>
2020-11-26 21:15:19 +0200

    added tabs and fixed bugs

--- app/js/main.js
+++ app/js/main.js
@@ -8,10 +8,10 @@ $(function() {
     $('.tab').on('click', function(e) {
         e.preventDefault();
         $($(this).siblings()).removeClass('tab--active');
-        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs__content--active');
 
         $(this).addClass('tab--active');
-        $($(this).attr('href')).addClass('tabs-content--active');
+        $($(this).attr('href')).addClass('tabs__content--active');
     });
 
 });
